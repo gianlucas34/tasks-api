d9cbf783668958c44365e8fa9c3f89790600fd44
Gian <gian@nullbug.dev>
2023-09-24 18:19:31 -0300

    feature: filter by priority

--- src/routes/tasks/get-all-tasks.ts
+++ src/routes/tasks/get-all-tasks.ts
@@ -1,9 +1,24 @@
 import { Request, Response } from 'express'
+import { Op } from 'sequelize'
 import { format } from 'date-fns'
 import { Task } from '../../models/Task'
 
-export const getAllTasksRoute = async (_: Request, response: Response) => {
-  const tasksFromDB = await Task.findAll({ raw: true })
+export const getAllTasksRoute = async (
+  request: Request,
+  response: Response
+) => {
+  const { priority } = request.query
+
+  const tasksFromDB = await Task.findAll({
+    raw: true,
+    ...(!!priority && {
+      where: {
+        priority: {
+          [Op.eq]: priority,
+        },
+      },
+    }),
+  })
   const tasks = tasksFromDB.map((task) => ({
     ...task,
     end_date: format(task.end_date, 'dd-MM-yyyy'),
